f0106a0e902586572c33edffef088f80ab2fe077
Gokmen Kilic <gkmnklc@gmail.com>
2025-04-01 12:54:50 +0100

    iso_fortran_env

---
+++ section1.01/exercise2.f90
@@ -0,0 +1,11 @@
+program exercise2
+
+        use, intrinsic :: iso_fortran_env, only: output_unit, error_unit, input_unit
+
+        print *, "output_unit is", output_unit
+        print *, "error_unit is", error_unit
+        print *, "input_unit is", input_unit
+        
+end program exercise2          
+
+
